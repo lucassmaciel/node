46b5cbfeea9e5633e440835b5ab7ee78dd621633
Lucas Maciel Gomes <lucasmacielg73@gmail.com>
2024-08-22 11:20:39 -0400

    node

---
+++ 02/acoes1.js
@@ -0,0 +1,17 @@
+let resp = window.document.getElementById('saida')
+
+function acao1(){
+    resp.innerHTML += "<p> Vc clicou no botao 1 </p>"
+}
+
+function acao2(){
+    resp.innerHTML += "<p> Vc clicou no botao 2 </p>"
+}
+
+function acao3(){
+    resp.innerHTML += "<p> Vc clicou no botao 3 </p>"
+}
+
+function acao4(){
+    resp.innerHTML += "<p> Vc clicou no botao 4 </p>"
+}
